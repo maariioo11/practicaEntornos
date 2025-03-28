9044193a9b54ed250356a6811d27080cfc498472
Alumno tarde <Alumno tarde@EQUIPO15>
2025-03-28 17:59:37 +0100

    Version 1.0

---
+++ practica4entornos/src/practica4entornos/programaPráctica_4.java
@@ -0,0 +1,19 @@
+package practica4entornos;
+
+public class programaPráctica_4 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Lista de los últimos 10 ganadores del balón de oro");
+		System.out.println("Ganador del Balón de Oro: Lionel Messi (2015)\r\n"
+				+ "Ganador del Balón de Oro: Cristiano Ronaldo (2016)\r\n"
+				+ "Ganador del Balón de Oro: Cristiano Ronaldo (2017)\r\n"
+				+ "Ganador del Balón de Oro: Luka Modric (2018)\r\n"
+				+ "Ganador del Balón de Oro: Lionel Messi (2019)\r\n"
+				+ "Ganador del Balón de Oro: Lionel Messi (2021)\r\n"
+				+ "Ganador del Balón de Oro: Karim Benzema (2022)\r\n"
+				+ "Ganador del Balón de Oro: Lionel Messi (2023)\r\n"
+				+ "Ganador del Balón de Oro: Rodri (2024)");
+	}
+
+}
