05a41c24e46282041224bc538e036a560c0d7ee4
Alumno tarde <Alumno tarde@EQUIPO15>
2025-04-01 15:59:43 +0200

    Desarrollo

--- practica4entornos/src/practica4entornos/programaPráctica_4.java
+++ practica4entornos/src/practica4entornos/programaPráctica_4.java
@@ -3,17 +3,22 @@ package practica4entornos;
 public class programaPráctica_4 {
 
 	public static void main(String[] args) {
+		Jugadores nuevo = new Jugadores ("Kylian Mbappe",2025);
 		
 		System.out.println("Lista de los últimos 10 ganadores del balón de oro");
-		System.out.println("Ganador del Balón de Oro: Lionel Messi (2015)\r\n"
-				+ "Ganador del Balón de Oro: Cristiano Ronaldo (2016)\r\n"
-				+ "Ganador del Balón de Oro: Cristiano Ronaldo (2017)\r\n"
-				+ "Ganador del Balón de Oro: Luka Modric (2018)\r\n"
-				+ "Ganador del Balón de Oro: Lionel Messi (2019)\r\n"
-				+ "Ganador del Balón de Oro: Lionel Messi (2021)\r\n"
-				+ "Ganador del Balón de Oro: Karim Benzema (2022)\r\n"
-				+ "Ganador del Balón de Oro: Lionel Messi (2023)\r\n"
-				+ "Ganador del Balón de Oro: Rodri (2024)");
+		System.out.println("Ganador del Balón de Oro: Lionel Messi 2015\r\n"
+				+ "Ganador del Balón de Oro: Cristiano Ronaldo 2016\r\n"
+				+ "Ganador del Balón de Oro: Cristiano Ronaldo 2017\r\n"
+				+ "Ganador del Balón de Oro: Luka Modric 2018\r\n"
+				+ "Ganador del Balón de Oro: Lionel Messi 2019\r\n"
+				+ "Ganador del Balón de Oro: Lionel Messi 2021\r\n"
+				+ "Ganador del Balón de Oro: Karim Benzema 2022\r\n"
+				+ "Ganador del Balón de Oro: Lionel Messi 2023\r\n"
+				+ "Ganador del Balón de Oro: Rodri (2024)\r\n"
+				+ "Ganador del Balón de Oro: "+nuevo.getNombre()+" " + nuevo.getAñoGanador());
+		
 	}
+	
+	
 
 }
